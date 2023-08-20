41b0201a6026d7087b2eeca7c85f1477caba6c24
hidekazu nasu <n-solutionshien@nscd.nstac.go.jp>
2023-08-20 19:50:36 +0900

    first commit

---
+++ .history/Program_20230820193249.cs
@@ -0,0 +1,62 @@
+using System.Net.Http;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/WeatherofTokyo", async () =>
+{
+    var url = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json";
+    HttpClient client = new HttpClient();
+    var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+    var response = await client.SendAsync(request);
+    string jsonResponse = await response.Content.ReadAsStringAsync();
+
+
+}
+);
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
---
+++ WeatherClass.cs
@@ -0,0 +1,9 @@
+
+public class Rootobject
+{
+    public string publishingOffice { get; set; }
+    public DateTime reportDatetime { get; set; }
+    public string targetArea { get; set; }
+    public string headlineText { get; set; }
+    public string text { get; set; }
+}
